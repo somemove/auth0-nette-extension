2ed864cf01ab8e3347537a50ec27f28d14526ce6
Josef Petrak <josef.petrak@somemove.cz>
2018-03-10 13:37:50 +0100

    Initial proposal of Auth0 Nette Extension

---
+++ src/NetteAuth0/NetteAuth0Extension.php
@@ -0,0 +1,32 @@
+<?php declare(strict_types=1);
+
+	namespace Somemove\NetteAuth0;
+
+	use \Nette\DI\Compiler;
+	use \Nette\DI\CompilerExtension;
+
+	final class NetteAuth0Extension extends CompilerExtension {
+
+		public $defaults = [
+			'response_mode' => 'query',
+			'response_type' => 'code',
+			'persist_user' => true,
+			'persist_access_token' => false,
+			'persist_refresh_token' => false,
+			'persist_id_token' => false,
+			'debug' => false,
+		];
+		
+		public function loadConfiguration() {
+			$builder = $this->getContainerBuilder();
+			$config = $this->getConfig($this->defaults);
+
+			$builder
+				->addDefinition('auth0')
+				->setClass('Auth0\SDK\Auth0')
+				->setArguments([$config]);
+		}
+
+	}
+
+?>
